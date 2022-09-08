b29d13b5f1811fa5c99fd541b2a81b1e6f7b71e2
harrelchris <24297899+harrelchris@users.noreply.github.com>
2022-09-08 05:45:07 -0500

    fix quotes

--- tests/models_test.py
+++ tests/models_test.py
@@ -2,8 +2,8 @@ from app.models import Item
 
 
 def test_item_attributes():
-    assert Item.__tablename__ == 'item'
-    assert hasattr(Item, 'boolean')
-    assert hasattr(Item, 'floating')
-    assert hasattr(Item, 'integer')
-    assert hasattr(Item, 'string')
+    assert Item.__tablename__ == "item"
+    assert hasattr(Item, "boolean")
+    assert hasattr(Item, "floating")
+    assert hasattr(Item, "integer")
+    assert hasattr(Item, "string")
