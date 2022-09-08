ab7f842cbb0b365af0ab2268f3f03f3ce96d56a4
harrelchris <24297899+harrelchris@users.noreply.github.com>
2022-09-08 05:50:19 -0500

    ignore flake8 unused error

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -4,8 +4,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-# Import all models here
-from app import database, models
+from app import database, models  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
