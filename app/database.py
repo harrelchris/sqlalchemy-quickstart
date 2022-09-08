90399f5d9ac2ae1ccfe8abdf62f4c23d6faad5e9
harrelchris <24297899+harrelchris@users.noreply.github.com>
2022-09-08 05:30:59 -0500

    refactor imports

--- app/database.py
+++ app/database.py
@@ -1,10 +1,11 @@
 import os
-from sqlalchemy import create_engine, func, Column, Integer, DateTime, MetaData
+
+from sqlalchemy import Column, DateTime, Integer, MetaData, create_engine, func
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
 from sqlalchemy_mixins import AllFeaturesMixin
 
-engine = create_engine(os.environ['DATABASE_URL'], echo=False)
+engine = create_engine(os.environ["DATABASE_URL"], echo=False)
 session = scoped_session(sessionmaker(bind=engine, autocommit=True))
 metadata = MetaData()
 
