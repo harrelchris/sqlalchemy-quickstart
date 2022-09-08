3f702bfef5d2373d6f1e9f5a354c2c3b0d774044
harrelchris <24297899+harrelchris@users.noreply.github.com>
2022-09-08 05:44:58 -0500

    fix quotes

--- tests/database_test.py
+++ tests/database_test.py
@@ -2,36 +2,36 @@ from app.database import Base
 
 
 def test_base_attributes():
-    assert Base.__tablename__ == 'base'
-    assert hasattr(Base, 'metadata')
-    assert hasattr(Base, 'session')
-    assert hasattr(Base, 'id')
-    assert hasattr(Base, 'created')
-    assert hasattr(Base, 'modified')
+    assert Base.__tablename__ == "base"
+    assert hasattr(Base, "metadata")
+    assert hasattr(Base, "session")
+    assert hasattr(Base, "id")
+    assert hasattr(Base, "created")
+    assert hasattr(Base, "modified")
 
 
 def test_base_methods():
-    assert hasattr(Base, 'all')
-    assert hasattr(Base, 'delete')
-    assert hasattr(Base, 'first')
-    assert hasattr(Base, 'save')
-    assert hasattr(Base, 'to_dict')
-    assert hasattr(Base, 'update')
+    assert hasattr(Base, "all")
+    assert hasattr(Base, "delete")
+    assert hasattr(Base, "first")
+    assert hasattr(Base, "save")
+    assert hasattr(Base, "to_dict")
+    assert hasattr(Base, "update")
 
 
 def test_instance_attributes(model):
-    assert model.__tablename__ == 'thing'
-    assert hasattr(model, 'metadata')
-    assert hasattr(model, 'session')
-    assert hasattr(model, 'id')
-    assert hasattr(model, 'created')
-    assert hasattr(model, 'modified')
+    assert model.__tablename__ == "thing"
+    assert hasattr(model, "metadata")
+    assert hasattr(model, "session")
+    assert hasattr(model, "id")
+    assert hasattr(model, "created")
+    assert hasattr(model, "modified")
 
 
 def test_instance_methods(model):
-    assert hasattr(model, 'all')
-    assert hasattr(model, 'delete')
-    assert hasattr(model, 'first')
-    assert hasattr(model, 'save')
-    assert hasattr(model, 'to_dict')
-    assert hasattr(model, 'update')
+    assert hasattr(model, "all")
+    assert hasattr(model, "delete")
+    assert hasattr(model, "first")
+    assert hasattr(model, "save")
+    assert hasattr(model, "to_dict")
+    assert hasattr(model, "update")
