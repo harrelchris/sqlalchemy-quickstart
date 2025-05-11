03a351120cb1c13349708adc3d44b5849f9db673
Chris Harrel <harrelchris@gmail.com>
2025-05-10 21:57:11 -0500

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,32 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.database import engine, Address, User
+
+# Create
+with Session(engine) as session:
+    spongebob = User(
+        name="spongebob",
+        fullname="Spongebob Squarepants",
+        addresses=[Address(email_address="spongebob@sqlalchemy.org")],
+    )
+
+    sandy = User(
+        name="sandy",
+        fullname="Sandy Cheeks",
+        addresses=[
+            Address(email_address="sandy@sqlalchemy.org"),
+            Address(email_address="sandy@squirrelpower.org"),
+        ],
+    )
+
+    patrick = User(name="patrick", fullname="Patrick Star")
+
+    session.add_all([spongebob, sandy, patrick])
+    session.commit()
+
+# Read
+with Session(engine) as session:
+    stmt = select(User).where(User.name.in_(["spongebob", "sandy"]))
+    for user in session.scalars(stmt):
+        print(user)
